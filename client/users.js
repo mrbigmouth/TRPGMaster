e3134a7b431c202c139fb8e28a37883baf4fa129
Yong Qian, Lee <b92101007@gmail.com>
2014-02-14 11:20:51 +0800

    加入Meteor.loginAs函數
    adm可以使用此函數直接變身指定使用�
    在本機測試時使用此函數登入為adm

---
+++ client/users.js
@@ -0,0 +1,7 @@
+Meteor.loginAs = function(user) {
+  Accounts.callLoginMethod(
+    {'methodArguments' : [ {'method' : 'loginAs', 'user' : user} ]
+    ,'userCallback'    : function() { console.log(arguments); }
+    }
+  );
+}
--- server/user.js
+++ server/user.js
@@ -32,3 +32,47 @@ Meteor.publish('users', function () {
 Meteor.publish('userNick', function () {
   return Meteor.users.find({}, {'fields': {'profile.nick' : 1}});
 });
+
+
+//Meteor.loginAs admin 登入指定使用者
+Accounts.registerLoginHandler(function(data) {
+  var token
+    , undefined
+    ;
+
+  if (data.method !== 'loginAs') {
+    return undefined;
+  }
+  if (Meteor.userId() === TRPG.adm && data.user) {
+    if (Meteor.users.findOne(data.user)) {
+      token = Accounts._generateStampedLoginToken();
+      Meteor.users.update(
+        data.user
+      , {$push : {'services.resume.loginTokens': token}
+        }
+      );
+      return {
+        'id'    : data.user
+      , 'token' : token.token
+      }
+    }
+    else {
+      throw new Meteor.Error(404, 'find no user!', 'no user id = ' + data.user);
+    }
+  }
+  else if (Meteor.absoluteUrl() === 'http://localhost:13667/') {
+    token = Accounts._generateStampedLoginToken();
+    Meteor.users.update(
+      TRPG.adm
+    , {$push : {'services.resume.loginTokens': token}
+      }
+    );
+    return {
+      id   : TRPG.adm
+    , token: token.token
+    }
+  }
+  else {
+    throw new Meteor.Error(401, 'auth fail!', 'only adm can use this function!');
+  }
+});
