fc96bcd5a038046c1d097dba99f9b68e69f13105
Yong Qian, Lee <b92101007@gmail.com>
2016-03-23 00:01:48 +0800

    暫時擺上從meteor server抓下來的壓縮code

--- client/users.js
+++
@@ -1,7 +0,0 @@
-Meteor.loginAs = function(user) {
-  Accounts.callLoginMethod(
-    {'methodArguments' : [ {'method' : 'loginAs', 'user' : user} ]
-    ,'userCallback'    : function() { console.log(arguments); }
-    }
-  );
-}
